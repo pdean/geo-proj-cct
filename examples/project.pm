3b5a2ff6585812b3528e74167b0a7ccb923f8629
peter dean <p.dean@internode.on.net>
2025-01-09 16:34:56 +1000

    moo example

--- examples/project.pm
+++ examples/project.pm
@@ -4,71 +4,42 @@ use v5.36;
 
 package Point;
 use Moo;
-use namespace::autoclean;
-use Types::Standard qw( Str Num );
 
-has id    => ( is => 'ro', isa => Str, required => 1 );
-has x     => ( is => 'ro', isa => Num, required => 1 );
-has y     => ( is => 'ro', isa => Num, required => 1 );
-has z     => ( is => 'ro', isa => Num, required => 1 );
-has desc  => ( is => 'ro', isa => Str, default  => '' );
-has style => ( is => 'ro', isa => Str, default  => '' );
+has id    => ( is => 'ro', required => 1 );
+has x     => ( is => 'ro', required => 1 );
+has y     => ( is => 'ro', required => 1 );
+has z     => ( is => 'ro', required => 1 );
+has desc  => ( is => 'ro', default  => '' );
+has style => ( is => 'ro', default  => '' );
 
 # -------------------------------------------
 
 package Style;
 use Moo;
-use namespace::autoclean;
-use Types::Standard qw( Str );
 
-has id    => ( is => 'ro', isa => Str, required => 1 );
-has color => ( is => 'ro', isa => Str, required => 1 );
-has href  => ( is => 'ro', isa => Str, required => 1 );
+has id    => ( is => 'ro', required => 1 );
+has color => ( is => 'ro', required => 1 );
+has href  => ( is => 'ro', required => 1 );
 
 # -------------------------------------------
 
 package Project;
 use Moo;
-use namespace::autoclean;
-use Types::Standard qw( Str ArrayRef InstanceOf );
-use Type::Params    qw( signature );
 use XML::Simple;
 
-has name => ( is => 'ro', isa => Str, required => 1 );
-
-has points => (
-    is      => 'ro',
-    isa     => ArrayRef [ InstanceOf ["Point"] ],
-    default => sub { return [] },
-);
-
-has styles => (
-    is      => 'ro',
-    isa     => ArrayRef [ InstanceOf ["Style"] ],
-    default => sub { return [] },
-);
+has name   => ( is => 'ro', required => 1 );
+has points => ( is => 'ro', default  => sub { return [] } );
+has styles => ( is => 'ro', default  => sub { return [] } );
 
 sub point_add {
-    state $check = signature(
-        method     => 1,
-        positional => [ InstanceOf ['Point'] ]
-    );
-
-    my ( $self, $point ) = $check->(@_);
+    my ( $self, $point ) = @_;
     push @{ $self->points }, $point;
-
     return $self;
 }
 
 sub style_add {
-    state $check = signature(
-        method     => 1,
-        positional => [ InstanceOf ['Style'] ]
-    );
-
-    my ( $self, $style ) = $check->(@_);
+    my ( $self, $style ) = @_;
     push @{ $self->styles }, $style;
-
     return $self;
 }
 
